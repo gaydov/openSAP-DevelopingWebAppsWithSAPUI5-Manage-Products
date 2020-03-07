779d184829ce6ea1a1be4128a586083d51461c49
Georgi Gaydov <gaidov@gmail.com>
2020-03-07 18:53:19 +0000

    Add week 3 exercises

---
+++ webapp/controller/ProductDetails.controller.js
@@ -0,0 +1,16 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"../model/formatter"
+], function (Controller, formatter) {
+	"use strict";
+
+	return Controller.extend("opensap.manageproducts.ManageProducts.controller.ProductDetails", {
+
+		formatter: formatter,
+
+		onInit: function () {
+			// this.byId("categoryLabel").setVisible(false);
+			// this.byId("category").setVisible(false);
+		}
+	});
+});
